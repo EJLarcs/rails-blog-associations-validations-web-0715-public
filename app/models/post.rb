647fce24ebc1f92a5f732da02612f1c83fd91b4d
EJLarcs <ejlarcombe@gmail.com>
2015-08-31 17:53:33 -0400

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
-  
+  #holds relationships/associations
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  # accepts_nested_attributes_for :tags
+  #holds validations
+  validates :name, presence: true
+  validates :content, presence: true
+
+
 end
